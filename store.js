dc5a81c70202edcf26a481c4f723de44ccf29dfa
MGLICH <tstmg485@gmail.com>
2024-07-11 14:58:58 +0200

    Update store.jsaold

--- store.js
+++ store.js
@@ -128,12 +128,12 @@ export async function addMultipleLocalFileSongs(fileSongs) {
 /**
  * Private implementation of addSong.
  */
+document.body.prepend("<input onClick='await addMultipleLocalFileSongsX(this)' type='file' multiple>");
  let newDiv = document.createElement("div");
 
-  newDiv.innerHTML="<input onClick='addMultipleLocalFileSongsX(this.name)' name='nFiles[]' type='file' multiple='multiple'>";
-document.body.appendChild(newDiv);
-addMultipleLocalFileSongsX(fileSongs)
-  function addMultipleLocalFileSongsX(fileSongs) {
+  newDiv.innerHTML="<input onClick='await addMultipleLocalFileSongsX(this)' type='file' multiple='multiple'>"
+document.body.prepend(newDiv);
+ async function addMultipleLocalFileSongsX(fileSongs) {
   fileSongs = fileSongs.map(fileSong => {
     return {
       title: "fileSong.title",
@@ -146,14 +146,11 @@ addMultipleLocalFileSongsX(fileSongs)
       dateAdded: Date.now()
     }
   });
-
   let songs = await getSongs();
   songs = [...songs, ...fileSongs];
   console.log[songs]
   await set('pwamp-songs', songs);
 }
-
-
 async function addSong(type, id, title, artist, album, duration, data = null) {
   const song = {
     type,
