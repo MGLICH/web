3ff497c3d3da69089aeb46f413095c8a57380311
MGLICH <tstmg485@gmail.com>
2024-07-11 10:23:15 +0200

    Update store.js

--- store.js
+++ store.js
@@ -1,39 +1,21 @@
-import { get, set } from './idb-keyval.js';
-import { getUniqueId } from './utils.js';
-
-export let wasStoreEmpty = false;
-
-// Songs are stored in IDB as an array under the 'songs' key.
-//
-// Songs have unique IDs to identify them. Songs also have a title, artist, album, and duration.
-// We do not read this information from the song file itself, this is stored in IDB too.
-// 
-// There are 2 types of songs: remote and file.
-// A remote song is one that has a URL to a remote audio file. A remote song's ID is its URL.
-// A file song is one that was loaded as a file from disk and stored in IDB. A file song's ID
-// is a unique ID generated when importing the file.
-
-/**
- * Get the list of all songs stored in IDB.
- */
 export async function getSongs() {
   let songs = await get('pwamp-songs');
-  var jsn
-  console.log(songs);
+
     try {
     let querystring = 'urlsong';
-     const response = await fetch('https://script.google.com/macros/s/AKfycbzbroaFPjbGeSDDe7GGqWSGxdrGzTdqzJ8V_rMCzTVNHSvMJJwgpVi60naqDW92IqIcQg/exec?page=api&q='+querystring)
-    
+     const response = await fetch('https://script.google.com/macros/s/AKfycbzbroaFPjbGeSDDe7GGqWSGxdrGzTdqzJ8V_rMCzTVNHSvMJJwgpVi60naqDW92IqIcQg/exec?page=api&q='+querystring, { 
+        signal: controller.signal 
+      });
+
       const html = await response.text();
-       jsn = await html;
+      const jsn = await html;
       console.log(JSON.stringify(JSON.parse(jsn)));
       console.log(JSON.parse(jsn));
     } catch (error) {
     console.log('Fetch error: ', error);
   } 
-   let newsongs = JSON.parse(jsn);
-   console.log('newsongs : ', newsongs);
-  
+   let newsongs = await jsn;
+
   await set('pwamp-songs', newsongs);
   if (!songs) {
     wasStoreEmpty = true;
@@ -76,14 +58,11 @@ export async function getSongs() {
       duration: '01:29',
       dateAdded: Date.now()
     }];
-
     await set('pwamp-songs', songs);
-
     // And store the artwork for those songs.
     await setArtwork('Noi2er', 'Beyond Reality (Vacuum) (LP)', 'https://ia803401.us.archive.org/11/items/DWK382/Noi2er_-_Beyond_Reality_Vacuum_Front.jpg');
     await setArtwork('David Rousset', 'Davrous Universe', 'https://microsoftedge.github.io/Demos/pwamp/songs/Reunion.jpg');
   }
-
   // Verify that all songs have the new dateAdded field,
   // If not, set it to the current date.
   for (let i = 0; i < songs.length; i++) {
@@ -92,15 +71,12 @@ export async function getSongs() {
       songs[i].dateAdded = Date.now();
       needToStore = true;
     }
-
     if (needToStore) {
       await set('pwamp-songs', songs);
     }
   }
-
   return songs;
 }
-
 /**
  * Get a song by its ID.
  */
@@ -108,7 +84,6 @@ export async function getSong(id) {
   const songs = await getSongs();
   return songs.find(song => song.id === id);
 }
-
 /**
  * Check if the given remote song URL is already in IDB.
  */
@@ -116,14 +91,12 @@ export async function hasRemoteURLSong(url) {
   const songs = await getSongs();
   return !!songs.find(s => s.id === url);
 }
-
 /**
  * Add a new remote song to the list of songs in IDB.
  */
 export async function addRemoteURLSong(url, title, artist, album, duration) {
   await addSong('url', url, title, artist, album, duration);
 }
-
 /**
  * DO NOT LOOP OVER THIS FUNCTION TO IMPORT SEVERAL FILES, THIS WILL LEAD TO
  * AN INCONSISTENT STORE STATE. USE addMultipleLocalFileSongs() INSTEAD.
@@ -134,7 +107,6 @@ export async function addLocalFileSong(file, title, artist, album, duration) {
   const id = getUniqueId();
   await addSong('file', id, title, artist, album, duration, file);
 }
-
 /**
  * Add several new file songs to the list of songs in IDB.
  */
@@ -151,12 +123,10 @@ export async function addMultipleLocalFileSongs(fileSongs) {
       dateAdded: Date.now()
     }
   });
-
   let songs = await getSongs();
   songs = [...songs, ...fileSongs];
   await set('pwamp-songs', songs);
 }
-
 /**
  * Private implementation of addSong.
  */
@@ -171,12 +141,10 @@ async function addSong(type, id, title, artist, album, duration, data = null) {
     dateAdded: Date.now(),
     data
   };
-
   let songs = await getSongs();
   songs.push(song);
   await set('pwamp-songs', songs);
 }
-
 /**
  * Given the unique ID to an existing song, edit its title, artist and album.
  */
@@ -186,14 +154,11 @@ export async function editSong(id, title, artist, album) {
   if (!song) {
     throw new Error(`Could not find song with id ${id}`);
   }
-
   song.title = title;
   song.artist = artist;
   song.album = album;
-
   await set('pwamp-songs', songs);
 }
-
 /**
  * Given the unique ID to an existing song, delete it from IDB.
  */
@@ -202,21 +167,17 @@ export async function deleteSong(id) {
   songs = songs.filter(song => song.id !== id);
   await set('pwamp-songs', songs);
 }
-
 /**
  * Delete all songs from IDB.
  */
 export async function deleteAllSongs() {
   await set('pwamp-songs', []);
 }
-
 export async function sortSongsBy(field) {
   if (['dateAdded', 'title', 'artist', 'album'].indexOf(field) === -1) {
     return;
   }
-
   let songs = await getSongs();
-
   songs = songs.sort((a, b) => {
     if (a[field] < b[field]) {
       return field === 'dateAdded' ? 1 : -1;
@@ -228,35 +189,30 @@ export async function sortSongsBy(field) {
   });
   await set('pwamp-songs', songs);
 }
-
 /**
  * Set the volume in IDB so that we can remember it next time.
  */
 export async function setVolume(volume) {
   await set('pwamp-volume', volume);
 }
-
 /**
  * Get the stored volume.
  */
 export async function getVolume() {
   return await get('pwamp-volume');
 }
-
 /**
  * Set a custom skin in IDB.
  */
 export async function setCustomSkin(skin) {
   await set('pwamp-customSkin', skin);
 }
-
 /**
  * Get the currently stored custom skin.
  */
 export async function getCustomSkin(skin) {
   return await get('pwamp-customSkin');
 }
-
 /**
  * Store a new artwork for the given artist and album.
  */
@@ -268,7 +224,6 @@ export async function setArtwork(artist, album, image) {
   artworks[`${artist}-${album}`] = image;
   await set('pwamp-artworks', artworks);
 }
-
 /**
  * Get the stored artworks.
  */
